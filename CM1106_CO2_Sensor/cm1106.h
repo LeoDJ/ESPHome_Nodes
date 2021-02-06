440e1cd956067583c8740efd6bd8af4f4d63e39f
mahler83 <33252024+SangzinAhn@users.noreply.github.com>
2021-02-06 23:17:23 +0900

    cm1106 zero calibration bug fix
    400ppm value is written to variable cmd instead of CM1106_CMD_SET_CO2_CALIB so the calibration is to 0ppm instead of 400ppm

--- CM1106_CO2_Sensor/cm1106.h
+++ CM1106_CO2_Sensor/cm1106.h
@@ -10,8 +10,8 @@ class CM1106 : public UARTDevice {
     void setCo2CalibValue(uint16_t ppm = 400) {
         uint8_t cmd[6];
         memcpy(cmd, CM1106_CMD_SET_CO2_CALIB, sizeof(cmd));
-        cmd[3] = ppm >> 8;
-        cmd[4] = ppm & 0xFF;
+        CM1106_CMD_SET_CO2_CALIB[3] = ppm >> 8;
+        CM1106_CMD_SET_CO2_CALIB[4] = ppm & 0xFF;
         uint8_t response[4] = {0};
         bool success = sendUartCommand(CM1106_CMD_SET_CO2_CALIB, sizeof(CM1106_CMD_SET_CO2_CALIB), response, sizeof(response));
 
